90552fb3515b968fac7c4ebd27fa7857d4fd7b0f
Arjya Sen <114721138+virus3-0@users.noreply.github.com>
2023-02-15 11:08:21 +0530

    Add files via upload

---
+++ src/main/java/com/example/telephonicDirectory/TelephonicDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.telephonicDirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephonicDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephonicDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/telephonicDirectory/TelephonicDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telephonicDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephonicDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
